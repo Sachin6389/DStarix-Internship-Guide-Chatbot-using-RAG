8abb939900bd5368511d9178884e42f6442d6223
Sachin6389 <Sachingond058@gmail.com>
2026-07-26 02:26:12 +0530

    porterror fixing

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from  src.search import RAGSearch
@@ -25,4 +25,8 @@ def chat():
     return jsonify({"status": "success", "response": response})
 
 if __name__ == "__main__":
-    app.run(debug=True ,host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
